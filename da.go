59793d5df26a8da6a5cd9da0b1c96f675d5f165d
vaca <wsmslgh@gmail.com>
2022-03-14 01:46:27 -0700

    Initial commit

---
+++ da.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Shopify/sarama"
+)
+
+func main() {
+	fmt.Printf("consumer_test \n")
+	config := sarama.NewConfig()
+	config.Consumer.Return.Errors = true
+	config.Version = sarama.V0_11_0_2
+	// consumer
+	consumer, err := sarama.NewConsumer([]string{"localhost:9092"}, config)
+	if err != nil {
+		fmt.Printf("consumer_test create consumer error %s\n", err.Error())
+		return
+	}
+	defer consumer.Close()
+	partitionConsumer, err := consumer.ConsumePartition("kafka_go_test", 0, sarama.OffsetOldest)
+	if err != nil {
+		fmt.Printf("try create partition_consumer error %s\n", err.Error())
+		return
+	}
+	defer partitionConsumer.Close()
+	for {
+		select {
+		case msg := <-partitionConsumer.Messages():
+			fmt.Printf("msg offset: %d, partition: %d, timestamp: %s, value: %s\n",
+				msg.Offset, msg.Partition, msg.Timestamp.String(), string(msg.Value))
+		case err := <-partitionConsumer.Errors():
+			fmt.Printf("err :%s\n", err.Error())
+		}
+	}
+
+}
